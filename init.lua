380d09d061d0d9a914c7182e267663a527364ea1
Harish-Chandar <149600195+Harish-Chandar@users.noreply.github.com>
2026-06-26 02:50:03 -0500

    html templates

--- init.lua
+++ init.lua
@@ -12,3 +12,19 @@ require("core.contributors")
 
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    pattern = "*.html",
+    callback = function()
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+        if #lines == 1 and lines[1] == "" then
+            
+            local home = os.getenv("HOME")
+            local template_path = home .. "/.config/nvim/html.template"
+            
+            vim.cmd("0r " .. template_path)
+            
+            vim.cmd("$delete")
+        end
+    end
+})
